f399d3d0c65e6ec03ae1902c3828a028d8d1ccfb
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-07-12 23:32:28 +0800

    feat: added base and auth thrift files
    + Added base file for base responses (status message and code)
    + Added idl file for authentication service

---
+++ auth/auth.thrift
@@ -0,0 +1,81 @@
+namespace go auth_service
+
+include "../base/base.thrift"
+
+enum UserRole {
+    UNKNOWN = 0
+    STUDENT = 1
+    TUTOR = 2
+}
+
+struct User {
+    1: string uid
+    2: string first_name
+    3: string last_name
+    4: string email
+    5: i64 created_at
+    6: i64 updated_at
+    7: UserRole role
+}
+
+struct LoginReq {
+    1: string email
+    2: string password
+    3: UserRole role
+}
+
+struct LoginRes {
+    1: base.BaseResponse base
+    2: optional User user
+    2: optional string jwt
+}
+
+struct RegisterReq {
+    1: base.BaseResponse base
+    2: string first_name
+    3: string last_name
+    4: string email
+    5: string password
+    6: UserRole role
+}
+
+struct RegisterRes {
+    1: base.BaseResponse base
+    2: optional User user
+    3: optional string jwt
+}
+
+struct ResetPasswordReq {
+    1: string email
+}
+
+struct ResetPasswordRes {
+    1: base.BaseResponse base
+}
+
+struct ResetPasswordVerifyReq {
+    1: string email
+    2: string code
+}
+
+struct ResetPasswordVerifyRes {
+    1: base.BaseResponse base
+}
+
+struct ResetPasswordSetReq {
+    1: string email
+    2: string code
+    3: string password
+}
+
+struct ResetPasswordSetRes {
+    1: base.BaseResponse base
+}
+
+service AuthService {
+    LoginRes login(1: LoginReq req)
+    RegisterRes register(1: RegisterReq req)
+    ResetPasswordRes reset_password(1: ResetPasswordReq req)
+    ResetPasswordVerifyRes reset_password_verify(1: ResetPasswordVerifyReq req)
+    ResetPasswordSetRes reset_password_set(1: ResetPasswordSetReq req)
+}
